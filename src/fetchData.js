4a0be8d5558789499eb029db7d0e6231e08205fe
Piotr Tomczyk <93157684+piotr-tomczyk@users.noreply.github.com>
2022-04-26 17:23:14 +0200

    CORS fix

--- src/fetchData.js
+++ src/fetchData.js
@@ -2,7 +2,9 @@ require("babel-core/register");
 require("babel-polyfill");
 export const getUserData = async () => {
   try {
-    const response = await fetch("https://fakestoreapi.com/users");
+    const response = await fetch("https://fakestoreapi.com/users", {
+      mode: "cors",
+    });
     const data = await response.json();
     return data;
   } catch (error) {
@@ -12,7 +14,9 @@ export const getUserData = async () => {
 
 export const getProductData = async () => {
   try {
-    const response = await fetch("https://fakestoreapi.com/products");
+    const response = await fetch("https://fakestoreapi.com/products", {
+      mode: "cors",
+    });
     const data = await response.json();
     return data;
   } catch (error) {
@@ -22,7 +26,9 @@ export const getProductData = async () => {
 
 export const getCartData = async () => {
   try {
-    const response = await fetch("https://fakestoreapi.com/carts");
+    const response = await fetch("https://fakestoreapi.com/carts", {
+      mode: "cors",
+    });
     const data = await response.json();
     return data;
   } catch (error) {
